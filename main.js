0ef451d536e81ca1ddf0edd9c6e5fcd7d2353456
luciano-repetti <lucianorepetti2020@gmail.com>
2022-12-11 02:02:02 -0300

    prueba dropdown

---
+++ main.js
@@ -0,0 +1,11 @@
+// const menuExpand = () =>{
+//     let submenu = document.getElementById("submenu")
+
+//     console.log("hola")
+
+//     if(submenu.className === "submenu"){
+//         submenu.classList.add("show")
+//     }else if(submenu.className === "submenu show"){
+//         submenu.classList.remove("show")
+//     }
+// }
